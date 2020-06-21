a4e28009c32adc00ef7a31e1d1e39b4a38474b5b
Sharee Burkeen <shareeburkeen@yahoo.com>
2020-06-21 15:46:47 -0500

    fixed player name bug

--- assets/js/game.js
+++ assets/js/game.js
@@ -155,8 +155,16 @@ var shop = function() {
   }
 };
 
+//function to get player name
+var getPlayerName = function() {
+  var name = "";
+  while (name === "" || name === null) {
+    name = prompt("What is your robot's name?");
+  }
+};
+
 var playerInfo = {
-  name: window.prompt("What is your robot's name?"),
+  name: getPlayerName(),
   health: 100,
   attack: 10,
   money: 10,
